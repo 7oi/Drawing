1d260cf00590f937790bf0e4a9e080ffcfc4225e
Arnór Bogason <arnor@arnor.is>
2014-01-31 16:17:07 +0000

    storage stuff n stuff

--- drawapp.js
+++ drawapp.js
@@ -104,29 +104,36 @@ function redrawAll() {
 
 // Clear: A way to clear the canvas
 $("#clear").click(function() {
+	clearCanvas();
+});
+
+$("#save").click(function saveImage() {
+
+	var saves = [];
+
+	if($.totalStorage('imgsaves')) {
+		saves = $.totalStorage('imgsaves');
+	}
+
+	saves.push({'name':'myDrawing','uri':canvas.toDataURL()});
+
+	$.totalStorage('imgsaves',saves);
+		
+})
+
+var loadImage = function(imguri){
+	clearCanvas();
+	var img = new Image;
+	img.onload = function(){
+	  context.drawImage(img,0,0); // Or at whatever offset you like
+	};
+	img.src = imguri;
+}
+
+var clearCanvas = function(){
 	canvas.width = canvas.width;
 	recall();
-});
-
-// Save: saving the image
-// Something fishy going on here
-$("#save").click(function saveImage() {
-	var data = canvas.toDataURL();
-
-	var request = new XMLHttpRequest();
-
-	request.onreadystatechange = function() {
-		if (request.readyState == 4 && request.status == 200) {
-			var response = request.responseText;
-			window.open(response, '_blank', 'location=0, menubar=0');
-		}
-	}
-	request.open('POST', 'save.php', true);
-	request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	request.send('img=' + data);
-
-	//window.open(data, '_blank', 'location=0, menubar=0');
-});
+}
 
 $("#undo").click(function() {
 	if (drawn.length > 0) {
